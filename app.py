81ac66bf263a2f45be0d28294749366710841932
Sean Rivera <vivereserive@gmail.com>
2026-07-30 10:13:39 +1200

    Addex index.html and fixed a few typos

--- app.py
+++ app.py
@@ -1,20 +1,26 @@
-from flask import Flask
+from flask import Flask, render_template
 from flask_sock import Sock
 
 # App Initialization
 app = Flask(__name__)
 sock = Sock(app)
 
+## Routes
+@app.route('/')
+def index():
+    return render_template('index.html')
+
 ## WebSocket Route
 # Reverses the String recieved from the Client
 @sock.route('/reverse')
 def reverse(ws):
     while True:
-        text = ws.recieve()
+        text = ws.receive()
+        if text is None:
+            break
         ws.send(text[::-1])
 
-# ws://<your_computer_ip>:5000/echo
+# ws://<your_computer_ip>:5000/reverse
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
 # Remove debug=True when in Production
-
